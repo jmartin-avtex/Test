612f6b30eceb42dcb183d0f14c05f67f79209d07
jmartin-avtex <31078595+jmartin-avtex@users.noreply.github.com>
2018-10-15 16:08:19 -0500

    Update BRICAUTHSTORAGE.js

--- BRICAUTHSTORAGE.js
+++ BRICAUTHSTORAGE.js
@@ -750,6 +750,173 @@ var BRICSALES;
 })(BRICSALES || (BRICSALES = {}));
 //# sourceMappingURL=EPX_BRICSALES.js.map
 
+var BRICREFUND;
+(function (BRICREFUND) {
+    var Functions = /** @class */ (function () {
+        function Functions() {
+        }
+        /*  -----------------------------------------------------------
+            PayPage JavaScript
+        --------------------------------------------------------------- */
+        Functions.prototype.initStyling = function () {
+            document.getElementById('amount').style.width = '8em';
+            document.getElementById('zip').style.width = '8em';
+            document.getElementById('check_nbr').style.width = '8em';
+            document.getElementById('amountRow').style.display = 'inline';
+            document.getElementById('chkNumRow').style.display = 'none';
+            document.getElementById('invoiceNumRow').style.display = 'none';
+            document.getElementById('radio1Row').style.display = 'none';
+            document.getElementById('userData1Row').style.display = 'none';
+            document.getElementById('acctTypeSRB').style.display = 'none';
+            document.getElementById('acctTypeSLbl').style.display = 'none';
+            document.getElementById('amountSpaceRow').style.display = 'inline';
+            document.getElementById('trantypeSpaceRow').style.display = 'inline';
+            document.getElementById('button2SpaceRow').style.display = 'inline';
+        };
+        /* Action to take when resetBtn is Clicked */
+        Functions.prototype.resetBtn_Click = function () {
+            document.getElementById('first_name').value = '';
+            document.getElementById('last_name').value = '';
+            document.getElementById('address').value = '';
+            document.getElementById('city').value = '';
+            document.getElementById('state').value = '';
+            document.getElementById('zip').value = '';
+            document.getElementById('phone_hm').value = '';
+            document.getElementById('account_nbr').value = '';
+            document.getElementById('routing_nbr').value = '';
+            document.getElementById('ach_account_nbr').value = '';
+            document.getElementById('monthDDL').selectedIndex = 0;
+            document.getElementById('yearDDL').selectedIndex = 0;
+            document.getElementById('exp_date').value = '';
+            document.getElementById('cvv2').value = '';
+            document.getElementById('invoice_nbr').value = '';
+            document.getElementById('user_data_1').value = '';
+            document.getElementById('declinedSP').innerHTML = '';
+            document.getElementById('miscMessageRow').style.display = 'none';
+            document.getElementById('miscMessageSpacerRow').style.display = 'none';
+            document.getElementById('first_name_msg').style.display = 'none';
+            document.getElementById('last_name_msg').style.display = 'none';
+            document.getElementById('address_msg').style.display = 'none';
+            document.getElementById('cityStateZip_msg').style.display = 'none';
+            document.getElementById('phone_hm_msg').style.display = 'none';
+            document.getElementById('phone_cell_msg').style.display = 'none';
+            document.getElementById('phone_wk_msg').style.display = 'none';
+            document.getElementById('user_data_1_msg').style.display = 'none';
+            document.getElementById('amount_msg').style.display = 'none';
+            document.getElementById('invoice_nbr_msg').style.display = 'none';
+            document.getElementById('account_nbr_msg').style.display = 'none';
+            document.getElementById('exp_date_msg').style.display = 'none';
+            document.getElementById('cvv2_msg').style.display = 'none';
+            document.getElementById('routing_nbr_msg').style.display = 'none';
+            document.getElementById('ach_account_nbr_msg').style.display = 'none';
+            document.getElementById('check_nbr_msg').style.display = 'none';
+        };
+        Functions.prototype.initPage = function () {
+            // Header Controls
+            document.getElementById('hdr0Lbl').innerHTML = 'Electronic Payment Exchange';
+            // see function hdrHprLnk_Click to set Redirect for hdrHprLnk
+            document.getElementById('hdrHprLnk').innerHTML = 'Close / Exit';
+            // Body Controls
+            document.getElementById('firstNameLbl').value = 'First Name:';
+            document.getElementById('lastNameLbl').value = 'Last Name:';
+            document.getElementById('addressLbl').value = 'Address:';
+            document.getElementById('cityStZipLbl').value = 'City,St,Zip:';
+            document.getElementById('phoneHMLbl').value = 'Home Phone:';
+            document.getElementById('submit2Btn').style.display = 'none';
+            document.getElementById('reset2Btn').style.display = 'none';
+            document.getElementById('cancel2Btn').style.display = 'none';
+            /*
+                EPX supports user data fields, fields our merchants can populate with information
+                of their choosing.  Define user data labels here.
+            */
+            document.getElementById('creditCardLbl').value = 'Card Number:';
+            document.getElementById('expDateLbl').value = 'Expiration Date:';
+            document.getElementById('cvv2Lbl').value = 'CVV Value:';
+            document.getElementById('rtNumLbl').value = 'Routing Number:';
+            document.getElementById('achAcctLbl').value = 'Account Number:';
+            document.getElementById('acctTypeLbl').value = 'Account Type:';
+            document.getElementById('acctTypeCLbl').value = 'Checking';
+            document.getElementById('acctTypeSLbl').value = 'Saving';
+            document.getElementById('chkNumLbl').value = 'Check Number:';
+            document.getElementById('amountLbl').value = 'Amount:';
+            amt = document.getElementById('amount');
+            amt.readOnly = 'readonly';
+            amt.tabIndex = -1; 
+            document.getElementById('amountLbl').style.display = "inline";  
+            document.getElementById('amount').style.border = '0';
+            document.getElementById('amount').style.display = 'inline';
+            document.getElementById('amountRow').style.display = "inline";
+            document.getElementById('user_data_1').style.display = "none";
+            document.getElementById('invoiceNumLbl').value = 'Invoice Number:';
+            document.getElementById('radioLbl').value = 'Transaction Type';
+            document.getElementById('radio1Lbl').value = 'Check Card';
+            document.getElementById('radio2Lbl').value = 'Credit';
+            document.getElementById('radio3Lbl').value = 'Debit';
+            document.getElementById('radio4Lbl').value = 'Debit'; // Pinless
+            document.getElementById('radio5Lbl').value = 'ACH';
+            // Disclaimer Text
+            document.getElementById('achDisclaimerTxt').value = 'BY CLICKING ON THE SUBMIT BUTTON, I AGREE TO THE TERMS AND CONDITIONS OF USING MY BANK ACCOUNT AS A PAYMENT METHOD, WHICH ARE LISTED BELOW, AND AUTHORIZE EPX (OR ITS AGENT) TO DEBIT MY BANK ACCOUNT FOR THE AMOUNT SPECIFIED.\n\nTerms and Conditions\n\nBy choosing to use a bank account as your method of payment, you will be able to complete your payment using any valid United States based financial institution automated clearing house ("ACH") enabled bank account. You are authorizing EPX (or its agent) to debit your bank account for the amount specified. To complete your transaction, EPX, or an agent acting on its behalf, will create an electronic funds transfer or bank draft, which will be presented to your bank or financial institution for payment from your bank account. You agree that: (a) you have read, understand and agree to these Terms and Conditions, and that this agreement constitutes a "writing signed by you" under any applicable law or regulation, (b) you consent to the electronic delivery of the disclosures contained in these Terms and Conditions, (c) you authorize EPX (or its agent) to initiate one or more ACH debit entries (withdrawals) for the specified amount(s) from your bank account, and you authorize the financial institution that holds your bank account to deduct such payments. EPX, in its sole discretion, may refuse this payment option service to anyone or any user without notice for any reason at any time.';
+            document.getElementById('debitDisclaimerTxt').innerHTML = 'NOTE: If you make payment with a debit card, the payment amount will be immediately deducted from your checking account.  The deduction cannot be reversed or voided upon deduction from your account.';
+            document.getElementById('miscCardImageLbl').value = 'We support the following financial networks:';
+            // Buttons Controls
+            document.getElementById('submitBtn').value = 'Submit';
+            document.getElementById('submit2Btn').value = 'Submit';
+            document.getElementById('resetBtn').value = 'Reset';
+            document.getElementById('reset2Btn').value = 'Reset';
+            document.getElementById('cancelBtn').value = 'Cancel';
+            document.getElementById('cancel2Btn').value = 'Cancel';
+            document.getElementById('swipeBtn').value = 'Swipe';
+            // Messaging Controls
+            document.getElementById('miscMessage1').innerHTML = 'Important Messages';
+            document.getElementById('miscMessage2').innerHTML = 'There was a problem with your request.';
+            // Footer Controls
+            document.getElementById('ftr0Lbl').innerHTML = "Powered by EPX (<a href='javascript:showEPX();'>www.epx.com</a>)";
+            document.getElementById('mainDiv').style.display = 'block';
+            document.getElementById('radio4Row').style.display = 'none';
+            document.getElementById('radio5Row').style.display = 'none';
+            document.getElementById('radio2RB').onclick = this.tranTypeRadioClick;
+            document.getElementById('radio2RB').checked = true;
+            Functions.prototype.eventFire(document.getElementById('radio2RB'), 'click');
+        };
+        Functions.prototype.eventFire = function (el, etype) {
+            if (el.fireEvent) {
+                el.fireEvent('on' + etype);
+            }
+            else {
+                var evObj = document.createEvent('Events');
+                evObj.initEvent(etype, true, false);
+                el.dispatchEvent(evObj);
+            }
+        };
+        Functions.prototype.tranTypeRadioClick = function (e) {
+            var evt = e ? e : (window.event) ? window.event : "";
+            if (evt) {
+                var elem = evt.target ? evt.target : evt.srcElement;
+                switch (elem.id) {
+                    case "radio2RB":
+                        document.getElementById('ccGroupDiv').style.display = 'inline';
+                        document.getElementById('achGroupDiv').style.display = 'none';
+                        break;
+                    case "radio3RB":
+                        document.getElementById('ccGroupDiv').style.display = 'inline';
+                        document.getElementById('achGroupDiv').style.display = 'none';
+                        break;
+                    case "radio3RB":
+                        document.getElementById('ccGroupDiv').style.display = 'inline';
+                        document.getElementById('achGroupDiv').style.display = 'none';
+                        break;
+                    case "radio5RB":
+                        document.getElementById('ccGroupDiv').style.display = 'none';
+                        document.getElementById('achGroupDiv').style.display = 'inline';
+                        break;
+                }
+            }
+        };
+        return Functions;
+    }());
+    BRICREFUND.Functions = Functions;
+})(BRICREFUND || (BRICREFUND = {}));
+//# sourceMappingURL=EPX_BRICREFUND.js.map
 
 
 
@@ -775,6 +942,8 @@ function resetBtn_Click() {
         BRICAUTH.Functions.prototype.resetBtn_Click();
     } else if (document.getElementById("user_data_7").value == "SALES" || document.getElementById("user_data_7").value == "SALE") {
         BRICSALES.Functions.prototype.resetBtn_Click();
+    } else if (document.getElementById("user_data_7").value == "REFUND") {
+        BRICREFUND.Functions.prototype.resetBtn_Click();
     }
 }
 
@@ -799,12 +968,15 @@ function initPage() {
     } else if (document.getElementById("user_data_7").value == "STORAGE-ORDER") {
         BRICORDERSTORAGE.Functions.prototype.initStyling();
         BRICORDERSTORAGE.Functions.prototype.initPage();
-    } else if (document.getElementById("user_data_7").value == "SALES" || document.getElementById("user_data_7").value == "SALE" || document.getElementById("user_data_7").value == "REFUND") {
+    } else if (document.getElementById("user_data_7").value == "SALES" || document.getElementById("user_data_7").value == "SALE") {
         BRICSALES.Functions.prototype.initStyling();
         BRICSALES.Functions.prototype.initPage();
     } else if (document.getElementById("user_data_7").value == "AUTH") {
         BRICAUTH.Functions.prototype.initStyling();
         BRICAUTH.Functions.prototype.initPage();
+    } else if (document.getElementById("user_data_7").value == "REFUND") {
+        BRICREFUND.Functions.prototype.initStyling();
+        BRICREFUND.Functions.prototype.initPage();
     }
 }
 
